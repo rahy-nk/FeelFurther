b373882b98d2d3a66e4ca1f89a340129cb4959bd
Rahini <rahink2008@outlook.com>
2023-08-15 11:23:27 -0400

    Initial Commit

---
+++ FeelFurther/FeelFurtherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelFurtherApp.swift
+//  FeelFurther
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FeelFurtherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
